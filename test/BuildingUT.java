2ac88cd8fcfaec0d05ce9759133b842cbbbaeade
Lukas Trimbacher <lukas.trimbacher@gmail.com>
2022-01-14 12:20:30 +0100

    UT buildingShouldNotBeNullAfterCreatingWithMoreInitialResidents

--- test/BuildingUT.java
+++ test/BuildingUT.java
@@ -7,4 +7,10 @@ public class BuildingUT {
         Building tower = new Building(1, "Josef");
         Assert.assertNotEquals(null, tower);
     }
+    @Test
+    public void buildingShouldNotBeNullAfterCreatingWithMoreInitialResidents(){
+        String[] residents = {"Josef", "Gabi", "Karl"};
+        Building tower = new Building(1, residents);
+        Assert.assertNotEquals(null, tower);
+    }
 }

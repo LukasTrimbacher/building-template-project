0b14bf06d0c43623dce573686a6ff34a41205f48
Lukas Trimbacher <lukas.trimbacher@gmail.com>
2022-01-14 12:26:54 +0100

    UT residentsSizeShouldBe3AfterCreatingWith3InitialResidents

--- test/BuildingUT.java
+++ test/BuildingUT.java
@@ -18,4 +18,10 @@ public class BuildingUT {
         Building tower = new Building(1, "Josef");
         Assert.assertEquals(1, (int) tower.getResidents());
     }
+    @Test
+    public void residentsSizeShouldBe3AfterCreatingWith3InitialResidents(){
+        String[] residents = {"Josef", "Gabi", "Karl"};
+        Building tower = new Building(1, residents);
+        Assert.assertEquals(3, (int) tower.getResidents());
+    }
 }
